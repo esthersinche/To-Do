d45945dbafe5e6fc741a6fa28b43affd0e1fd2c3
BeeIsStr <156618675+esthersinche@users.noreply.github.com>
2025-06-26 23:08:58 -0500

    creado

---
+++ src/app/components/tarea-form/tarea-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tarea-form',
+  imports: [],
+  templateUrl: './tarea-form.component.html',
+  styleUrl: './tarea-form.component.css'
+})
+export class TareaFormComponent {
+
+}
